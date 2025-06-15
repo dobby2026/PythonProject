d9468621ef41b78ae379f0341b2244bc81d01aab
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-06-15 16:53:01 +0900

    반복문

---
+++ section06/Ex06-03-while.py
@@ -0,0 +1,12 @@
+"""
+파일명: Ex06-03-while.py
+"""
+my_list = []
+n = 1
+while n != 0:
+    n = int(input('정수를 입력하세요(종료는 0입니다.) >>> '))
+    my_list.append(n)
+
+print(my_list)
+my_list.pop()
+print(my_list)
