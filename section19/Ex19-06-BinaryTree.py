9d3caed90da6cbc75fd8db09b178b1f111ccfd43
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-07-12 16:18:17 +0900

    section19

--- section19/Ex19-06-BinaryTree.py
+++ section19/Ex19-06-BinaryTree.py
@@ -27,6 +27,10 @@ class BinaryTree:
         self.root = TreeNode(root)  # 루트 노드
 
     def insert(self, value):
+        '''
+        value = 4
+        '''
+
 
         if not self.root:
             self.root = TreeNode(value)
@@ -34,7 +38,10 @@ class BinaryTree:
            self._insert(value, self.root)
 
     def _insert(self, value, current_node):
-
+        '''
+        value = 4
+        current_node = TreeNode(5)
+        '''
         if value < current_node.value:
             if not current_node.left:
                 current_node.left = TreeNode(value)
@@ -50,6 +57,15 @@ class BinaryTree:
         else:
             print('이미 존재하는 값입니다.')
 
+    def preorder(self, start, traversal):
+
+        if start:
+            traversal += (str(start.value) + '#')
+            traversal = self.preorder(start.left, traversal)
+            traversal = self.preorder(start.right, traversal)
+
+        return traversal
+
 
 # 실행코드
 bt = BinaryTree(5)  # 루트 노드 5인 이진트리 객체 생성
@@ -62,6 +78,8 @@ bt.insert(4)
 bt.insert(6)
 bt.insert(8)
 
+# 이진 트리를 전위 순회한 결과 출력
+print(f'전위 순회: {bt.preorder(bt.root, '')}')
 
 
 
