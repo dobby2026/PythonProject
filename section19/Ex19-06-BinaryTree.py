e153b566c226a7d9f4e5256e28bf1012f0036ca1
hyuniverse-dev <hyuniverse.dev@gmail.com>
2025-07-12 15:31:13 +0900

    section19

---
+++ section19/Ex19-06-BinaryTree.py
@@ -0,0 +1,68 @@
+"""
+파일명: Ex19-06-BinaryTree.py
+
+트리 자료구조
+    단 하나의 루트 노드가 있고, 루트 노드에서 하위 노드들이 연결된
+    비선형 계층구조이다.
+
+이진트리 (Binary Tree)
+    모든 노드가 최대 2개의 자식 노드를 가질 수 있는 구조를 말한다.
+
+    예) 기준 - 왼쪽 서브트리의 값은 루트의 값보다 작고, 오른쪽 서브트리의 값은 루트보다
+            큰 값을 가지도록 구성
+
+"""
+
+class TreeNode:
+
+    def __init__(self, value):
+        self.value = value  # 노드의 값
+        self.left = None    # 왼쪽 서브트리 노드
+        self.right = None   # 오른쪽 서브트리 노드
+
+
+class BinaryTree:
+
+    def __init__(self, root):
+        self.root = TreeNode(root)  # 루트 노드
+
+    def insert(self, value):
+
+        if not self.root:
+            self.root = TreeNode(value)
+        else:
+           self._insert(value, self.root)
+
+    def _insert(self, value, current_node):
+
+        if value < current_node.value:
+            if not current_node.left:
+                current_node.left = TreeNode(value)
+            else:
+                self._insert(value, current_node.left)
+
+        elif value > current_node.value:
+            if not current_node.right:
+                current_node.right = TreeNode(value)
+            else:
+                self._insert(value, current_node.right)
+
+        else:
+            print('이미 존재하는 값입니다.')
+
+
+# 실행코드
+bt = BinaryTree(5)  # 루트 노드 5인 이진트리 객체 생성
+
+# 값 삽입
+bt.insert(3)
+bt.insert(7)
+bt.insert(2)
+bt.insert(4)
+bt.insert(6)
+bt.insert(8)
+
+
+
+
+
